06d17f30a0b77441cd1e12236a3493f0d872d051
limberc <zehua.cheng@cs.ox.ac.uk>
2023-01-02 17:48:07 +0000

    Reformat the datamodules.py

--- gradattack/datamodules.py
+++ gradattack/datamodules.py
@@ -10,8 +10,8 @@ from torch.utils.data import Subset
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.sampler import Sampler
-from torchvision.datasets.cifar import CIFAR10, CIFAR100
 from torchvision.datasets import MNIST
+from torchvision.datasets.cifar import CIFAR10, CIFAR100
 
 DEFAULT_DATA_DIR = "./data"
 DEFAULT_NUM_WORKERS = 32
@@ -105,40 +105,44 @@ class FileDataModule(LightningDataModule):
         return self.get_dataloader()
 
 
-class ImageNetDataModule(LightningDataModule):
+class BaseDataModule(LightningDataModule):
     def __init__(
             self,
             augment: dict = None,
-            data_dir: str = os.path.join(DEFAULT_DATA_DIR, "imagenet"),
+            data_dir: str = DEFAULT_DATA_DIR,
             batch_size: int = 32,
             num_workers: int = DEFAULT_NUM_WORKERS,
             batch_sampler: Sampler = None,
             tune_on_val: bool = False,
     ):
+        super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.num_classes = 1000
-        self.multi_class = False
 
         self.batch_sampler = batch_sampler
         self.tune_on_val = tune_on_val
-
+        self._train_transforms = self.train_transform(augment)
+        print(self._train_transforms)
+        self._test_transforms = self.init_transform
         print(data_dir)
-        imagenet_normalize = transforms.Normalize((0.485, 0.456, 0.406),
-                                                  (0.229, 0.224, 0.225))
 
-        self._train_transforms = [
+    @property
+    def init_transform(self):
+        return [
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            imagenet_normalize,
+            DATASET_NORM[self.DATASET_NAME],
         ]
+
+    def train_transform(self, augment):
+        train_transforms = self.init_transform
         if augment["hflip"]:
-            self._train_transforms.insert(
+            train_transforms.insert(
                 0, transforms.RandomHorizontalFlip(p=0.5))
         if augment["color_jitter"] is not None:
-            self._train_transforms.insert(
+            train_transforms.insert(
                 0,
                 transforms.ColorJitter(
                     brightness=augment["color_jitter"][0],
@@ -148,20 +152,45 @@ class ImageNetDataModule(LightningDataModule):
                 ),
             )
         if augment["rotation"] > 0:
-            self._train_transforms.insert(
+            train_transforms.insert(
                 0, transforms.RandomRotation(augment["rotation"]))
         if augment["crop"]:
-            self._train_transforms.insert(0,
-                                          transforms.RandomCrop(32, padding=4))
+            train_transforms.insert(0, transforms.RandomCrop(32, padding=4))
+        return train_transforms
 
-        print(self._train_transforms)
+    @property
+    def num_classes(self):
+        return None
 
-        self._test_transforms = [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            imagenet_normalize,
-        ]
+    def train_dataloader(self):
+        if self.batch_sampler is None:
+            return DataLoader(self.train_set,
+                              batch_size=self.batch_size,
+                              num_workers=self.num_workers)
+        else:
+            return DataLoader(
+                self.train_set,
+                batch_sampler=self.batch_sampler,
+                num_workers=self.num_workers,
+            )
+
+    def val_dataloader(self):
+        return DataLoader(self.val_set,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_set,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers)
+
+
+class ImageNetDataModule(BaseDataModule):
+    DATASET_NAME = 'imagenet'
+
+    @property
+    def num_classes(self):
+        return 1000
 
     def setup(self, stage: Optional[str] = None):
         """Initialize the dataset based on the stage option ('fit', 'test' or 'attack'):
@@ -208,30 +237,8 @@ class ImageNetDataModule(LightningDataModule):
                 ori_train_set)
             self.train_set = Subset(ori_train_set, self.attack_indices)
 
-    def train_dataloader(self):
-        if self.batch_sampler is None:
-            return DataLoader(self.train_set,
-                              batch_size=self.batch_size,
-                              num_workers=self.num_workers)
-        else:
-            return DataLoader(
-                self.train_set,
-                batch_sampler=self.batch_sampler,
-                num_workers=self.num_workers,
-            )
 
-    def val_dataloader(self):
-        return DataLoader(self.val_set,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_set,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
-
-
-class MNISTDataModule(LightningDataModule):
+class MNISTDataModule(BaseDataModule):
     DATASET_NAME = 'mnist'
 
     def __init__(
@@ -243,56 +250,23 @@ class MNISTDataModule(LightningDataModule):
             batch_sampler: Sampler = None,
             tune_on_val: float = 0,
     ):
-        super().__init__()
+        super().__init__(augment, data_dir, batch_size, num_workers, batch_sampler, tune_on_val)
         self._has_setup_attack = False
-
-        self.data_dir = data_dir
-        self.batch_size = batch_size
-        self.num_workers = num_workers
         self.dims = (3, 32, 32)
-        self.num_classes = 10
-
-        self.batch_sampler = batch_sampler
-        self.tune_on_val = tune_on_val
         self.multi_class = False
-        mnist_normalize = DATASET_NORM[self.DATASET_NAME]
 
-        self._train_transforms = [
+    @property
+    def init_transform(self):
+        return [
             transforms.Resize(32),
             transforms.Grayscale(3),
             transforms.ToTensor(),
-            mnist_normalize,
-        ]
-        if augment["hflip"]:
-            self._train_transforms.insert(
-                0, transforms.RandomHorizontalFlip(p=0.5))
-        if augment["color_jitter"] is not None:
-            self._train_transforms.insert(
-                0,
-                transforms.ColorJitter(
-                    brightness=augment["color_jitter"][0],
-                    contrast=augment["color_jitter"][1],
-                    saturation=augment["color_jitter"][2],
-                    hue=augment["color_jitter"][3],
-                ),
-            )
-        if augment["rotation"] > 0:
-            self._train_transforms.insert(
-                0, transforms.RandomRotation(augment["rotation"]))
-        if augment["crop"]:
-            self._train_transforms.insert(0,
-                                          transforms.RandomCrop(32, padding=4))
-
-        print(self._train_transforms)
-
-        self._test_transforms = [
-            transforms.Resize(32),
-            transforms.Grayscale(3),
-            transforms.ToTensor(),
-            mnist_normalize,
+            DATASET_NORM[self.DATASET_NAME],
         ]
 
-        self.prepare_data()
+    @property
+    def num_classes(self):
+        return 10
 
     def prepare_data(self):
         MNIST(self.data_dir, train=True, download=True)
@@ -369,34 +343,8 @@ class MNISTDataModule(LightningDataModule):
             self.train_set = Subset(ori_train_set, self.attack_indices)
             self.test_set = Subset(self.test_set, range(100))
 
-    def train_dataloader(self):
-        if self.batch_sampler is None:
-            return DataLoader(
-                self.train_set,
-                batch_size=self.batch_size,
-                num_workers=self.num_workers,
-                shuffle=True,
-            )
-        else:
-            return DataLoader(
-                self.train_set,
-                batch_sampler=self.batch_sampler,
-                num_workers=self.num_workers,
-                shuffle=True,
-            )
 
-    def val_dataloader(self):
-        return DataLoader(self.val_set,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_set,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
-
-
-class CIFAR10DataModule(LightningDataModule):
+class CIFAR10DataModule(BaseDataModule):
     DATASET_NAME = 'cifar10'
 
     def __init__(
@@ -409,52 +357,26 @@ class CIFAR10DataModule(LightningDataModule):
             tune_on_val: float = 0,
             seed: int = None,
     ):
-        super().__init__()
+        super().__init__(augment, data_dir, batch_size, num_workers, batch_sampler,
+                         tune_on_val)
         self._has_setup_attack = False
-
-        self.data_dir = data_dir
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.dims = (3, 32, 32)
-        self.num_classes = 10
-
-        self.batch_sampler = batch_sampler
-        self.tune_on_val = tune_on_val
-        self.multi_class = False
         self.seed = seed
-        cifar_normalize = DATASET_NORM[self.DATASET_NAME]
 
-        self._train_transforms = [transforms.ToTensor(), cifar_normalize]
-        if augment["hflip"]:
-            self._train_transforms.insert(
-                0, transforms.RandomHorizontalFlip(p=0.5))
-        if augment["color_jitter"] is not None:
-            self._train_transforms.insert(
-                0,
-                transforms.ColorJitter(
-                    brightness=augment["color_jitter"][0],
-                    contrast=augment["color_jitter"][1],
-                    saturation=augment["color_jitter"][2],
-                    hue=augment["color_jitter"][3],
-                ),
-            )
-        if augment["rotation"] > 0:
-            self._train_transforms.insert(
-                0, transforms.RandomRotation(augment["rotation"]))
-        if augment["crop"]:
-            self._train_transforms.insert(0,
-                                          transforms.RandomCrop(32, padding=4))
-
-        print(self._train_transforms)
-
-        self._test_transforms = [transforms.ToTensor(), cifar_normalize]
-
+        self.dims = (3, 32, 32)
+        self.multi_class = False
         self.prepare_data()
+
+    @property
+    def init_transform(self):
+        return [transforms.ToTensor(), DATASET_NORM[self.DATASET_NAME]]
+
+    @property
+    def num_classes(self):
+        return 10
 
     def prepare_data(self):
         """Download the data"""
-        CIFAR10(self.data_dir, train=True, download=True)
-        CIFAR10(self.data_dir, train=False, download=True)
+        CIFAR10(self.data_dir, download=True)
 
     def setup(self, stage: Optional[str] = None):
         """Initialize the dataset based on the stage option ('fit', 'test' or 'attack'):
@@ -527,31 +449,13 @@ class CIFAR10DataModule(LightningDataModule):
             self.train_set = Subset(ori_train_set, self.attack_indices)
             self.test_set = Subset(self.test_set, range(100))
 
-    def train_dataloader(self):
-        if self.batch_sampler is None:
-            return DataLoader(self.train_set,
-                              batch_size=self.batch_size,
-                              num_workers=self.num_workers)
-        else:
-            return DataLoader(
-                self.train_set,
-                batch_sampler=self.batch_sampler,
-                num_workers=self.num_workers,
-            )
 
-    def val_dataloader(self):
-        return DataLoader(self.val_set,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_set,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
 class CIFAR100DataModule(CIFAR10DataModule):
     DATASET_NAME = 'cifar100'
 
+    def __init__(self):
+        self.num_classes = 100
+
     def prepare_data(self):
         """Download the data"""
-        CIFAR100(self.data_dir, train=True, download=True)
-        CIFAR100(self.data_dir, train=False, download=True)
+        CIFAR100(self.data_dir, download=True)
